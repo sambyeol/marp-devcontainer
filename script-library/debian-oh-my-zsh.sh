8278b0b2d53271011e722e2ff7b13408a0489af1
Henry Lee <gbvrcx@gmail.com>
2023-07-10 14:02:52 +0900

    feat: use gentoo as default theme

--- script-library/debian-oh-my-zsh.sh
+++ script-library/debian-oh-my-zsh.sh
@@ -2,5 +2,5 @@ USE_OMZ=${1:-"true"}
 
 if [ "${USE_OMZ}" = "true" ] ; then
     RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    sed -i -e "s/ZSH_THEME=.*/ZSH_THEME=\"simple\"/g" ~/.zshrc
+    sed -i -e "s/ZSH_THEME=.*/ZSH_THEME=\"gentoo\"/g" ~/.zshrc
 fi
